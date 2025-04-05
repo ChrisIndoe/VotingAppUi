a7dfb4c56e55c3fa16daac416119eec357e4e957
Chris Indoe <chris.indoe@gmail.com>
2025-04-05 10:10:10 -0400

    Make work on desktop and mobile

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-
 import { render } from 'react-dom'
 import Example from './example'
 import { DndProvider } from 'react-dnd'
 import { TouchBackend } from 'react-dnd-touch-backend'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 function App() {
+    // Check if the device is mobile
+    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    
     return (
         <div className="App">
-            <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
+            <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
                 <Example />
             </DndProvider>
         </div>
